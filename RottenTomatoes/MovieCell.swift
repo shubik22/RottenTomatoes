2cd3bf1171ad480621cf5c081a1b286bd97c8f84
sam <sam@wealthfront.com>
2015-09-17 21:58:02 -0700

    Set up MovieCell

---
+++ RottenTomatoes/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  RottenTomatoes
+//
+//  Created by Sam Sweeney on 9/17/15.
+//  Copyright © 2015 Wealthfront. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
