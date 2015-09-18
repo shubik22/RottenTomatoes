329f5d6d88f7b280b467b3966134022d0043ef17
sam <sam@wealthfront.com>
2015-09-17 21:29:45 -0700

    Load data and put title into cell

---
+++ RottenTomatoes/MoviesViewController.swift
@@ -0,0 +1,64 @@
+//
+//  MoviesViewController.swift
+//  RottenTomatoes
+//
+//  Created by Sam Sweeney on 9/17/15.
+//  Copyright © 2015 Wealthfront. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var movies: [NSDictionary]?
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")
+        let task =  NSURLSession.sharedSession().dataTaskWithRequest(
+            NSURLRequest(URL: url!),
+            completionHandler: {
+                (data, response, error) -> Void in
+                if let data = data {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        do {
+                            if let json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0)) as? NSDictionary {
+                                self.movies = json["movies"] as? [NSDictionary]
+                                self.tableView.reloadData()
+                            }
+                        } catch {
+                            print("Error loading json....")
+                        }
+                    }
+                    
+                } else if let error = error {
+                    print(error.description)
+                }
+                
+        })
+        task.resume()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! UITableViewCell
+
+        let movie = movies![indexPath.row]
+        cell.textLabel?.text = movie["title"] as! String
+        print(movie["title"] as! String)
+        return cell
+    }
+
+}
