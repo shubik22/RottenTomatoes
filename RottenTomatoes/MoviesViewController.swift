4dbce3f29f16082c6393c7f0aafa5e93d7af8244
sam <sam@wealthfront.com>
2015-09-19 23:37:55 -0700

    Add UIRefreshControl

--- RottenTomatoes/MoviesViewController.swift
+++ RottenTomatoes/MoviesViewController.swift
@@ -11,13 +11,49 @@ import UIKit
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var movies: [NSDictionary]?
+    var refreshControl: UIRefreshControl!
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var errorView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        loadMovies()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
 
+        let movie = movies![indexPath.row]
+        cell.titleLabel.text = movie["title"] as! String
+        cell.synopsisLabel.text = movie["synopsis"] as! String
+        
+        let posterUrl = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
+        cell.posterView.setImageWithURL(posterUrl)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func loadMovies() {
         let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")
         let task =  NSURLSession.sharedSession().dataTaskWithRequest(
             NSURLRequest(URL: url!),
@@ -49,34 +85,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 
         })
         task.resume()
+    }
 
-        tableView.dataSource = self
-        tableView.delegate = self
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let movies = movies {
-            return movies.count
-        } else {
-            return 0
-        }
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
-
-        let movie = movies![indexPath.row]
-        cell.titleLabel.text = movie["title"] as! String
-        cell.synopsisLabel.text = movie["synopsis"] as! String
-        
-        let posterUrl = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
-        cell.posterView.setImageWithURL(posterUrl)
-        
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    func onRefresh() {
+        loadMovies()
+        self.refreshControl.endRefreshing()
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
